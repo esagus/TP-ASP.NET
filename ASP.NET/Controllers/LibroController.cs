5010872d6556b19c1e0898512f0ad798a92a35e2
orteg <orteg@DESKTOP-JBLSFAU>
2021-11-25 21:30:04 -0300

    Agregar archivos de proyecto.

---
+++ ASP.NET/Controllers/LibroController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.Entity;
+using System.Web;
+using ASP.NET.Models;
+using System.Web.Mvc;
+using System.Net;
+
+namespace ASP.NET.Controllers
+{
+    public class LibroController : Controller
+    {
+        // GET: Libro
+        private LibroTpEntities1 db = new LibroTpEntities1();
+        public ActionResult Index()
+        {
+            try
+            {
+                using (var db = new LibroTpEntities1())
+                {
+                    return View(db.Libro.ToList());
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+       
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = db.Libro.FirstOrDefault(l => l.Id == id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Titulo,Autor,CantidadPaginas,ISBN,Edicion,Editorial, CiudadPais,FechaEdicion")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Libro.Add(libro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(libro);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = db.Libro.Find(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Titulo,Autor,CantidadPaginas,ISBN,Edicion,Editorial, CiudadPais,FechaEdicion")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(libro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(libro);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = db.Libro.Find(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+        [HttpPost, ActionName("Eliminar")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Libro libro = db.Libro.Find(id);
+            db.Libro.Remove(libro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
